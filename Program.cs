8a6119a254f92b349283e6d9df369ca105f71043
LipeClash <felipeclaudino2003@hotmail.com>
2020-05-13 15:51:53 -0300

    feito

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Leet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entrada	Saída");
+            Console.WriteLine("A ou a	4");
+            Console.WriteLine("E ou e	3");
+            Console.WriteLine("I ou i ou L ou l	1");
+            Console.WriteLine("O ou o	0");
+            Console.WriteLine("T ou t	7");
+            Console.WriteLine("S ou s	5");
+
+            String TraduçaõLeet = "Maria foi ate a feira\nM4r14 f01 473 4 f31r4";
+            Console.WriteLine(TraduçaõLeet);
+        }
+    }
+}
